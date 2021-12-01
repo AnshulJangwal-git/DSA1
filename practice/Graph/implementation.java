ec187d5d367fb4fc647e77c9dea0a27d21d6147b
anshul <anshuljangwal006@gmail.com>
2021-12-01 15:16:48 +0530

    up

---
+++ practice/Graph/implementation.java
@@ -0,0 +1,44 @@
+package Graph;
+import java.util.* ;
+import java.io.* ;
+
+public class implementation {
+    static class Edge{
+        int src ;
+        int dest ;
+        int wt ;
+
+        Edge(int src, int dest, int wt){
+            this.src = src ;
+            this.dest = dest ;
+            this.wt = this.wt ;
+        }
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)) ;
+        int vtces = Integer.parseInt(br.readLine()) ;
+
+        ArrayList<Edge>[] graph = new ArrayList[vtces] ;
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>() ;
+        }
+
+        int edges = Integer.parseInt(br.readLine()) ;
+        for(int i = 0; i < edges; i++){
+            String parts[] = br.readLine().split(" ") ;
+            int v1 = Integer.parseInt(parts[0]) ;
+            int v2 = Integer.parseInt(parts[1]) ;
+            int wt = Integer.parseInt(parts[2]) ;
+
+            graph[v1].add(new Edge(v1, v2, wt)) ;
+            graph[v2].add(new Edge(v2, v1, wt)) ;
+        }
+
+        int src = Integer.parseInt(br.readLine()) ;
+        int des = Integer.parseInt(br.readLine()) ;
+
+    }    
+}
